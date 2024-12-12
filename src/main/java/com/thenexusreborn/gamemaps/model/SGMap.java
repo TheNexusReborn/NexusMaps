4eeda3aff2a247648934135376688cfc630cd05e
Firestar311 <slaterandrew0398@gmail.com>
2024-12-12 12:55:51 -0600

    Some updates related to libraries

--- src/main/java/com/thenexusreborn/gamemaps/model/SGMap.java
+++ src/main/java/com/thenexusreborn/gamemaps/model/SGMap.java
@@ -1,9 +1,9 @@
 package com.thenexusreborn.gamemaps.model;
 
+import com.stardevllc.helper.FileHelper;
 import com.stardevllc.starcore.utils.Cuboid;
 import com.stardevllc.starcore.utils.Position;
 import com.stardevllc.starcore.utils.ServerProperties;
-import com.stardevllc.starlib.helper.FileHelper;
 import com.thenexusreborn.api.sql.annotations.column.ColumnCodec;
 import com.thenexusreborn.api.sql.annotations.column.ColumnIgnored;
 import com.thenexusreborn.api.sql.annotations.column.ColumnType;
